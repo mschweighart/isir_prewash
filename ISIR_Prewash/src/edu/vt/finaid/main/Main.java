b5c9e755e1869d6ba91abd154bec6ce8dd370e64
Schweighart <mschweig@vt.edu>
2018-05-02 13:48:13 -0400

    Adding files

---
+++ ISIR_Prewash/src/edu/vt/finaid/main/Main.java
@@ -0,0 +1,9 @@
+package edu.vt.finaid.main;
+import edu.vt.finaid.gui.GUI;
+
+public class Main {
+	public static void main(String[] args) {
+		GUI mainGUI = new GUI();
+		mainGUI.setVisible(true);
+	}
+}
---
+++ ISIR_Prewash/src/edu/vt/finaid/sql/SQL.java
@@ -0,0 +1,8 @@
+package edu.vt.finaid.sql;
+import edu.vt.finaid.gui.GUI;
+
+public class SQL {
+	public SQL() {
+		
+	}
+}
